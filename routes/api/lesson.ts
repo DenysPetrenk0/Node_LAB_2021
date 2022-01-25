e019ca6b911fc759250b60bffc23b25378af981e
Denys Petrenko <p.gesser2015@gmail.com>
2022-01-25 23:35:42 +0200

    HW_04: homework done

--- routes/api/lesson.ts
+++ routes/api/lesson.ts
@@ -1,38 +1,45 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
-import operations from '../../model';
+const pool = require("../../dataBase/db");
 
 router.get("/", (req, res, next) => {
     try {
-        const contacts = operations.getLessons();
-        res.json({
-            status: "success",
-            code: 200,
-            data: {
-                result: contacts,
-            },
+        pool.query("SELECT * FROM lessons", (err, data) => {
+            if (err) {
+                return console.log(err);
+            }
+            res.json({
+                status: "success",
+                code: 200,
+                data: {
+                    result: data,
+                },
+            });
         });
     } catch (error) {
         next(error);
     }
 });
 
-router.patch("/:lessonId", (req, res, next) => {
+router.patch("/lesson:id", (req, res, next) => {
     try {
-        const result = operations.updateLesson(req.body);
-        if (!result) {
-            return res.status(404).json({
-                status: "error",
-                code: 404,
-                message: "Not found",
+        const id = req.params.id;
+        pool.query("UPDATE lessons WHERE id=?", [id], (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            if (!id) {
+                return res.status(404).json({
+                    status: "error",
+                    code: 404,
+                    message: "Not found",
+                });
+            }
+            res.json({
+                status: "success",
+                code: "200",
+                data: data,
             });
-        }
-        res.json({
-            status: "success",
-            code: "200",
-            data: {
-                result,
-            },
         });
     } catch (error) {
         next(error);
@@ -41,14 +48,28 @@ router.patch("/:lessonId", (req, res, next) => {
 
 router.post("/lesson", (req, res, next) => {
     try {
-        const result = operations.createLesson(req.body);
-        res.status(201).json({
-            status: "success",
-            code: 201,
-            data: {
-                result,
-            },
-        });
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+        const lessonName = req.body.lessonName;
+        const course = req.body.course;
+        const teacher = req.body.teacher;
+
+        pool.query("INSERT INTO lessons (lessonName, course, teacher) VALUES (?,?,?)",
+            [
+                lessonName,
+                course,
+                teacher,
+            ], (err, data) => {
+                if (err) {
+                    return console.log(err);
+                }
+                res.status(201).json({
+                    status: "success",
+                    code: 201,
+                    data: data,
+                });
+            });
     } catch (error) {
         next(error);
     }
--- routes/api/user.ts
+++ routes/api/user.ts
@@ -1,17 +1,20 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
-import operations from '../../model';
-
+const pool = require("../../dataBase/db");
 
 router.get("/", (req, res, next) => {
     try {
-        const contacts = operations.getUsers();
-        res.json({
-            status: "success",
-            code: 200,
-            data: {
-                result: contacts,
-            },
+        pool.query("SELECT * FROM users", (err, data) => {
+            if (err) {
+                return console.log(err);
+            }
+            res.json({
+                status: "success",
+                code: 200,
+                data: {
+                    result: data,
+                },
+            });
         });
     } catch (error) {
         next(error);
@@ -20,14 +23,34 @@ router.get("/", (req, res, next) => {
 
 router.post("/admin", (req, res, next) => {
     try {
-        const result = operations.createAdmin(req.body);
-        res.status(201).json({
-            status: "success",
-            code: 201,
-            data: {
-                result,
-            },
-        });
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+        const firstName = req.body.firstName;
+        const secondName = req.body.secondName;
+        const age = req.body.age;
+        const gender = req.body.gender;
+        const role = req.body.role;
+        const isAdmin = req.body.isAdmin;
+
+        pool.query("INSERT INTO users (firstName, secondName, age, gender, role, isAdmin) VALUES (?,?,?,?,?,?)",
+            [
+                firstName,
+                secondName,
+                age,
+                gender,
+                role,
+                isAdmin
+            ], (err, data) => {
+                if (err) {
+                    return console.log(err);
+                }
+                res.status(201).json({
+                    status: "success",
+                    code: 201,
+                    data: data,
+                });
+            });
     } catch (error) {
         next(error);
     }
@@ -35,14 +58,36 @@ router.post("/admin", (req, res, next) => {
 
 router.post("/teacher", (req, res, next) => {
     try {
-        const result = operations.createTeacher(req.body);
-        res.status(201).json({
-            status: "success",
-            code: 201,
-            data: {
-                result,
-            },
-        });
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+        const firstName = req.body.firstName;
+        const secondName = req.body.secondName;
+        const age = req.body.age;
+        const gender = req.body.gender;
+        const role = req.body.role;
+        const specialization = req.body.specialization;
+        const grade = req.body.grade;
+
+        pool.query("INSERT INTO users (firstName, secondName, age, gender, role, specialization, grade) VALUES (?,?,?,?,?,?,?)",
+            [
+                firstName,
+                secondName,
+                age,
+                gender,
+                role,
+                specialization,
+                grade
+            ], (err, data) => {
+                if (err) {
+                    return console.log(err);
+                }
+                res.status(201).json({
+                    status: "success",
+                    code: 201,
+                    data: data,
+                });
+            });
     } catch (error) {
         next(error);
     }
@@ -50,24 +95,50 @@ router.post("/teacher", (req, res, next) => {
 
 router.post("/student", (req, res, next) => {
     try {
-        const result = operations.createStudent(req.body);
-        res.status(201).json({
-            status: "success",
-            code: 201,
-            data: {
-                result,
-            },
-        });
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+        const firstName = req.body.firstName;
+        const secondName = req.body.secondName;
+        const age = req.body.age;
+        const gender = req.body.gender;
+        const role = req.body.role;
+        const faculty = req.body.faculty;
+        const course = req.body.course;
+
+        pool.query("INSERT INTO users (firstName, secondName, age, gender, role, faculty, course) VALUES (?,?,?,?,?,?,?)",
+            [
+                firstName,
+                secondName,
+                age,
+                gender,
+                role,
+                faculty,
+                course
+            ], (err, data) => {
+                if (err) {
+                    return console.log(err);
+                }
+                res.status(201).json({
+                    status: "success",
+                    code: 201,
+                    data: data,
+                });
+            });
     } catch (error) {
         next(error);
     }
 });
 
-router.delete("/user", (req, res, next) => {
+router.delete("/user/:id", (req, res, next) => {
     try {
-        const { userId } = req.params;
-        const result = operations.deleteUser(userId);
-        if (!result) {
+        const id = req.params.id;
+        pool.query("DELETE FROM users WHERE id=?", [id], (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        if (!id) {
             return res.status(404).json({
                 status: "error",
                 code: 404,
